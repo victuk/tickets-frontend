03e0ec998250fdca9a032ddd8368d89c53b1f395
Victor <ukokjnr@gmail.com>
2023-02-25 00:42:59 +0100

    Updated

--- js/js/allHelpers.js
+++ js/js/allHelpers.js
@@ -1,5 +1,5 @@
-// const baseUrl = "http://localhost:5000/v1/";
-const baseUrl = "https://ticket-backend-ki4u.onrender.com/v1/";
+const baseUrl = "http://localhost:5000/v1/";
+// const baseUrl = "https://ticket-backend-ki4u.onrender.com/v1/";
 
 const auth = {
     login: `${baseUrl}auth/login`,
